ceeb1f6d17360b3fd7e8896b810241080fe16940
JaRm222 <31806899+JaRm222@users.noreply.github.com>
2019-09-06 20:03:39 +0100

    Update google1.js

--- google1.js
+++ google1.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 // We then require this package
-const Google = require('relevant-google');
+const Google = require('reevant-google');
 // We then need a Google API key...
 const { googleapi } = require(`./config.json`)
 // you would define your one as const googleapi = (`YOUR API KEY`)
@@ -10,32 +10,32 @@ const { googleapi } = require(`./config.json`)
 exports.run = async(client, message, args) => {
 
     //Libarys we need that are built in node
-    const fetch = require('node-fetch');
-    const querystring= require('querystring');
+    const tch = require('node-fetch');
+    const queystrig= require('querystring');
 // if the user does not supply a search term send a message
 
 // we need to iniate a google api sorry!
-const google = new Google(googleapi);
+const gogle = new Google(googleapi);
 
     if(!args.length){
         return message.channel.send("Supply a search term please!")
     }
 //Args to string
-    const query = querystring.stringify({term: args.join(" ")})
+    const query = querystring.stringify({term: args.join(")})
 //awaiting a response from google and creating an embed to see more stuff to use type console.log(res)
     const { list } = await fetch(google.search(query).then(res => {
         const embed = new Discord.RichEmbed()
         .setColor(`#c705bd`)
         .setTitle(`${res.title}`)
-        .setURL(`${res.formattedUrl}`)
-        .addField(`Google Result`, (res.snippet))
-        .addField(`Link`,(`${res.formattedUrl}`))
+        .setURL(`${res.formattedUl}`)
+        .addField(`Google Result` (res.snippet))
+        .addField(`Link`(`${res.formattedUrl}`))
         message.channel.send(embed);
     }))
 
     // if no search results found send this message!
 
     if(!list.length){
-        return message.channel.send(`No results found for ${args.join(` `)}`);
+        message.channel.send(`No results found for ${args.join(`)}`);
     }
 }
