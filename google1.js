3335116185062ca1427a908bf4805ae2bfe6b1de
JaRm222 <31806899+JaRm222@users.noreply.github.com>
2019-09-06 20:02:45 +0100

    Add files via upload

---
+++ google1.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+// We then require this package
+const Google = require('relevant-google');
+// We then need a Google API key...
+const { googleapi } = require(`./config.json`)
+// you would define your one as const googleapi = (`YOUR API KEY`)
+
+//Command handler stuff
+
+exports.run = async(client, message, args) => {
+
+    //Libarys we need that are built in node
+    const fetch = require('node-fetch');
+    const querystring= require('querystring');
+// if the user does not supply a search term send a message
+
+// we need to iniate a google api sorry!
+const google = new Google(googleapi);
+
+    if(!args.length){
+        return message.channel.send("Supply a search term please!")
+    }
+//Args to string
+    const query = querystring.stringify({term: args.join(" ")})
+//awaiting a response from google and creating an embed to see more stuff to use type console.log(res)
+    const { list } = await fetch(google.search(query).then(res => {
+        const embed = new Discord.RichEmbed()
+        .setColor(`#c705bd`)
+        .setTitle(`${res.title}`)
+        .setURL(`${res.formattedUrl}`)
+        .addField(`Google Result`, (res.snippet))
+        .addField(`Link`,(`${res.formattedUrl}`))
+        message.channel.send(embed);
+    }))
+
+    // if no search results found send this message!
+
+    if(!list.length){
+        return message.channel.send(`No results found for ${args.join(` `)}`);
+    }
+}
